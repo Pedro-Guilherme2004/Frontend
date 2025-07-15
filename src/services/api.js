ba38c423282f283e65719514eb08e8dc818cb908
Pedro Guilherme <cubomagico135z@gmail.com>
2025-07-15 20:45:48 -0300

    fix: atualiza api.js para usar JWT automaticamente

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://geticard.onrender.com/api", // sem barra no final!
+  baseURL: "https://geticard.onrender.com",
+});
+
+// Adiciona automaticamente o JWT nas requisições
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers["Authorization"] = "Bearer " + token;
+  }
+  return config;
 });
 
 export default api;
+
 
